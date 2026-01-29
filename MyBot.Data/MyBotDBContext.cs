c20bc304f9a840f8363ab6859b2b2ba532b0de3a
Mert_1026 <mertelsenev10@gmail.com>
2026-01-29 12:06:12 +0200

    Add project files.

---
+++ MyBot.Data/MyBotDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBot.Data
+{
+    internal class MyBotDBContext : DbContext
+    {
+        public MyBotDBContext(DbContextOptions<MyBotDBContext> options)
+        : base(options)
+        {
+
+        }
+    }
+}
